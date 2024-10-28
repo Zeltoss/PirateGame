5f58f6b4e2eebc9e4d7e264f3b272433e5c5f31e
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2024-10-28 05:51:24 +0100

    Main Menu Tweaks
    - Added potentiel Button Sounds to Main Menu
    - Added Rope Asset
    - Added OnClick Method to ButtonSounds Script
    - Small changes to Button size in Main Menu

--- Assets/Scripts/UI/ButtonSounds.cs
+++ Assets/Scripts/UI/ButtonSounds.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 public class ButtonSounds : MonoBehaviour
 {
     [SerializeField] private AudioClip[] hoverSounds;
+    [SerializeField] private AudioClip[] ClickSounds;
 
     public void OnPointerEnter()
     {
         SoundFXManager.instance.PlayRandomSoundFXClip(hoverSounds, transform, 1f);
+    }
+
+    public void OnClick()
+    {
+        SoundFXManager.instance.PlayRandomSoundFXClip(ClickSounds, transform, 1f);
     }
 }
